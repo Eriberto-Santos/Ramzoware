eb654625f5803fd5643bf36e7dde5b1a88b58020
Eriberto-SG <eribertosg.dev@gmail.com>
2022-10-30 17:28:36 -0600

    Se agrego la funcionalidad para encontrar archivos dentro de un directorio

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,16 @@
-import os 
+from cgi import print_directory
+import os
 from cryptography.fernet import Fernet
 
+# *Encontrar archivos en el directorio actual
+
+files = []
 
 
+for file in os.listdir():
+    if file == "encrypt.py" or file == ".gitignore":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
 
+print(files)
